7fa8124593e60fb370a94466523571f39d7bcc11
Alexander Borysov <borysovalexander@gmail.com>
2014-03-24 04:00:36 +0200

    Added evolution rate tracking Added 'offline' run capability Normalised city coordinates and cost calculations. Costs are now displayed as if the window was at the (default) size of 640x480, regardless of actual size. Visualisation also resizes correctly during a run now. Made nodes red, because they are now badass. Added vim magic modelines to a few files.

--- src/City.java
+++ src/City.java
@@ -1,14 +1,17 @@
 class City {
 
+  private static final int WINDOW_WIDTH = 630; //getBounds().width - 10
+  private static final int WINDOW_HEIGHT = 412; //ctrlButtons.getBounds().y - fm.getHeight() - 2
+
   /**
    * The city's x position.
    */
-  private int xpos;
+  private double xpos;
 
   /**
    * The city's y position.
    */
-  private int ypos;
+  private double ypos;
 
   /**
    * Constructor.
@@ -16,7 +19,7 @@ class City {
    * @param x The city's x position
    * @param y The city's y position.
    */
-  public City(int x, int y) {
+  public City(double x, double y) {
     xpos = x;
     ypos = y;
   }
@@ -26,7 +29,7 @@ class City {
    *
    * @return The city's x position.
    */
-  public int getx() {
+  public double getx() {
     return xpos;
   }
 
@@ -35,8 +38,16 @@ class City {
    *
    * @return The city's y position.
    */
-  public int gety() {
+  public double gety() {
     return ypos;
+  }
+
+  public int getProjectedX(int width) {
+    return (int)(xpos * width);
+  }
+
+  public int getProjectedY(int height) {
+    return (int)(ypos * height);
   }
 
   /**
@@ -58,10 +69,11 @@ class City {
    * @param y The y coordinate
    * @return The distance.
    */
-  public double proximity(int x, int y) {
-    int xdiff = xpos - x;
-    int ydiff = ypos - y;
+  public double proximity(double x, double y) {
+    double xdiff = (WINDOW_WIDTH * xpos) - (WINDOW_WIDTH * x);
+    double ydiff = (WINDOW_HEIGHT * ypos) - (WINDOW_HEIGHT * y);
     // TODO: Save a sqrt today
     return Math.sqrt(xdiff * xdiff + ydiff * ydiff);
   }
 }
+// vim: ts=2:sw=2
--- src/TravelingSalesman.java
+++ src/TravelingSalesman.java
@@ -9,7 +9,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.Queue;
+import java.util.LinkedList;
 
 /**
  * This class implements the Traveling Salesman problem
@@ -20,7 +21,7 @@ public class TravelingSalesman extends Applet implements Runnable {
   /**
    * How many cities to use.
    */
-	protected int cityCount;
+  protected int cityCount;
 
   /**
    * How many chromosomes to use.
@@ -68,6 +69,11 @@ public class TravelingSalesman extends Applet implements Runnable {
   private Button ctrlStart;
 
   /**
+   * The Magic button.
+   */
+  private Button ctrlMagic;
+
+  /**
    * The TextField that holds the number of cities.
    */
   private TextField ctrlCities;
@@ -88,11 +94,15 @@ public class TravelingSalesman extends Applet implements Runnable {
    */
   private String status = "";
 
-  private ArrayBlockingQueue<Double> convergenceHistory;
+  private Queue<Double> convergenceHistory;
+  private Queue<Long> timingHistory;
+
+  private int HISTORY_SIZE = 100;
 
   private final Random random = new Random(System.currentTimeMillis());
 
-  private static final int MIN_PARENT_POOL_SIZE = 130;
+  //private static final int MIN_PARENT_POOL_SIZE = 80;
+  private Integer minParentPoolSize = null;
   private static final int ELITISM = 0;
 
   @Override
@@ -102,22 +112,31 @@ public class TravelingSalesman extends Applet implements Runnable {
     // setup the controls
     ctrlButtons = new Panel();
     ctrlStart = new Button("Start");
+    ctrlMagic = new Button("Magic");
     ctrlButtons.add(ctrlStart);
     ctrlButtons.add(new Label("# Cities:"));
     ctrlButtons.add(ctrlCities = new TextField(5));
     ctrlButtons.add(new Label("Population Size:"));
     ctrlButtons.add(ctrlPopulationSize = new TextField(5));
+    ctrlButtons.add(ctrlMagic);
     this.add(ctrlButtons, BorderLayout.SOUTH);
 
     // set the default values
     ctrlPopulationSize.setText("1000");
-    ctrlCities.setText("50");
+    ctrlCities.setText("200");
 
     // add an action listener for the button
     ctrlStart.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        startThread();
+        startThreadFromUI();
+      }
+    });
+
+    ctrlMagic.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        startThreadFromUI();
       }
     });
 
@@ -128,31 +147,43 @@ public class TravelingSalesman extends Applet implements Runnable {
   /**
    * Start the background thread.
    */
-  private void startThread() {
-	  try {
-		  cityCount = Integer.parseInt(ctrlCities.getText());
-	  } catch (NumberFormatException e) {
-		  cityCount = 50;
-	  }
+  private void startThreadFromUI() {
+    try {
+      cityCount = Integer.parseInt(ctrlCities.getText());
+    } catch (NumberFormatException e) {
+      cityCount = 50;
+    }
 
-	  try {
-		  populationSize = Integer.parseInt(ctrlPopulationSize.getText());
-	  } catch (NumberFormatException e) {
-		  populationSize = 1000;
-	  }
+    try {
+      populationSize = Integer.parseInt(ctrlPopulationSize.getText());
+      minParentPoolSize = 70;//500;//(int) (0.6 * populationSize);//(int)(0.15 * populationSize * cityCount/70.0);
+    } catch (NumberFormatException e) {
+      populationSize = 1000;
+    }
+    startThread(cityCount, populationSize, minParentPoolSize);
+  }
 
-	  FontMetrics fm = getGraphics().getFontMetrics();
-	  int bottom = ctrlButtons.getBounds().y - fm.getHeight() - 2;
+  private void startThread(int citCount, int population, int poolsize) {
+    cityCount = citCount;
+
+    populationSize = population;
+    minParentPoolSize = poolsize;
+
+    FontMetrics fm = getGraphics().getFontMetrics();
+    int bottom = ctrlButtons.getBounds().y - fm.getHeight() - 2;
 
     // create a random list of cities
     cities = new City[cityCount];
     for (int i = 0; i < cityCount; i++) {
       cities[i] = new City(
-        (int) (Math.random() * (getBounds().width - 10)),
-        (int) (Math.random() * (bottom - 10)));
+          (double) (Math.random()),
+          //(int) (Math.random() * (getBounds().width - 10)),
+          (double) (Math.random()));
+          //(int) (Math.random() * (bottom - 10)));
     }
 
-    convergenceHistory = new ArrayBlockingQueue<Double>(100);
+    convergenceHistory = new LinkedList<Double>();
+    timingHistory = new LinkedList<Long>();
 
     // create the initial population of chromosomes
     chromosomes = new Chromosome[populationSize];
@@ -187,30 +218,35 @@ public class TravelingSalesman extends Applet implements Runnable {
     int width = getBounds().width;
     int bottom = ctrlButtons.getBounds().y - fm.getHeight() - 2;
 
+    int drawableWidth = width - 10;
+    int drawableHeight = bottom - 10;
+
     g.setColor(Color.black);
     g.fillRect(0, 0, width, bottom);
 
     if(started && cities != null) {
-	    g.setColor(Color.green);
-	    for (int i = 0; i < cityCount; i++) {
-	      int xpos = cities[i].getx();
-	      int ypos = cities[i].gety();
-	      g.fillOval(xpos - 5, ypos - 5, 10, 10);
-	    }
+      g.setColor(Color.red);
+      for (int i = 0; i < cityCount; i++) {
+        int xpos = cities[i].getProjectedX(drawableWidth);
+        //int xpos = cities[i].getx();
+        int ypos = cities[i].getProjectedY(drawableHeight);
+        //int ypos = cities[i].gety();
+        g.fillOval(xpos - 5, ypos - 5, 10, 10);
+      }
 
-	    g.setColor(Color.white);
-	    for (int i = 0; i < cityCount; i++) {
+      g.setColor(Color.white);
+      for (int i = 0; i < cityCount; i++) {
         Chromosome chromosome = chromosomes[0];
-	      int icity = chromosome.getCity(i);
-	      if (i != 0) {
-	        int last = chromosome.getCity(i - 1);
-	        g.drawLine(
-	                  cities[icity].getx(),
-	                  cities[icity].gety(),
-	                  cities[last].getx(),
-	                  cities[last].gety());
-	      }
-	    }
+        int icity = chromosome.getCity(i);
+        if (i != 0) {
+          int last = chromosome.getCity(i - 1);
+          g.drawLine(
+              cities[icity].getProjectedX(drawableWidth),
+              cities[icity].getProjectedY(drawableHeight),
+              cities[last].getProjectedX(drawableWidth),
+              cities[last].getProjectedY(drawableHeight));
+        }
+      }
 
     }
 
@@ -234,10 +270,34 @@ public class TravelingSalesman extends Applet implements Runnable {
    */
   @Override
   public void run() {
-    double thisCost = 500.0;
+    //feed all our UI-supplied parameters to the GA
+    runGA(true, 10000, populationSize, minParentPoolSize);
+    /*
+    int[] ar = {50, 100, 150, 250, 400, 600, 1000, 2000, 3000, 5000};
+    //RunResult[] resses = new RunResult[ar.length];
+    int numSamples = 5;
+    for(int i = 0; i < ar.length; i++) { //for each population size
+      int pSize = ar[i];
+      RunResult averaged = new RunResult();
+      for(int j = 0; j < numSamples; j++) {
+        averaged.add(runGA(false, 10000, pSize, (int)(0.5 * pSize)));
+      }
+      averaged.divide(numSamples);
+      System.out.println("For population size " + pSize + ":");
+      System.out.println("Average cost: " + averaged.cost);
+      System.out.println("Average convergence generation: " + averaged.convergenceGen);
+      System.out.println("Average rate: " + averaged.rate);
+      System.out.println();
+    }
+    */
+  }
 
+  @Override
+  public void paint(Graphics g) {
     update();
+  }
 
+  public RunResult runGA(boolean graphical, int maxGenerations, int popSize, int poolSize) {
     final int n = populationSize;
     final float inversePop = 1.0f / n;
     final float nMinus = 0.5f;
@@ -246,10 +306,25 @@ public class TravelingSalesman extends Applet implements Runnable {
 
     Set<Integer> parentPool = new HashSet<Integer>();
     List<Chromosome> childPool = new ArrayList<Chromosome>();
+    long genStartTime = -1;
+    RunResult result = new RunResult();
 
-    for (; generation <= 1000; generation++) {
+    NumberFormat doubf = NumberFormat.getInstance();
+    doubf.setMinimumFractionDigits(0);
+    doubf.setMaximumFractionDigits(2);
+    NumberFormat intf = NumberFormat.getInstance();
+    intf.setMaximumFractionDigits(0);
+    intf.setGroupingUsed(false);
+
+    if(graphical) {
+      setStatus("Simulation starting up...");
+      update();
+    }
+
+    for (; generation <= maxGenerations; generation++) {
+      genStartTime = System.currentTimeMillis();
       parentPool.clear();
-      while (parentPool.size() < MIN_PARENT_POOL_SIZE) {
+      while (parentPool.size() < poolSize) {
         for (int i = 1; i <= n - ELITISM; i++) {
           float probability = inversePop * (nMinus + nDiff * (i - 1) / (n - 1));
           if (random.nextFloat() <= probability) {
@@ -289,47 +364,75 @@ public class TravelingSalesman extends Applet implements Runnable {
       }
 
       Arrays.sort(chromosomes);
-      thisCost = chromosomes[0].getCost();
-
-      NumberFormat nf = NumberFormat.getInstance();
-      nf.setMinimumFractionDigits(2);
-      nf.setMinimumFractionDigits(2);
+      result.cost = chromosomes[0].getCost();
 
       //convergence detection
-      try {
-          if(convergenceHistory.remainingCapacity() == 0)convergenceHistory.take();
-      } catch(InterruptedException e) {
-          throw new Error("Zomg!");
+      if(convergenceHistory.size() == HISTORY_SIZE) {
+        convergenceHistory.remove();
       }
-      convergenceHistory.add(thisCost);
-      if(convergenceHistory.remainingCapacity() == 0){
-          double sum = 0;
-          for(double i : convergenceHistory) {
-              sum+=i;
+      convergenceHistory.add(result.cost);
+      if(convergenceHistory.size() == HISTORY_SIZE){
+        double sum = 0;
+        for(double i : convergenceHistory) {
+          sum += i;
+        }
+        double avg = sum/convergenceHistory.size();
+        double sumOfSquares = -1;
+        for(double i : convergenceHistory) {
+          double diff = Math.abs(i - avg);
+          sumOfSquares += diff * diff;
+        }
+        double stdDev = Math.sqrt(sumOfSquares);
+        if(stdDev < 50) {
+          if(graphical) {
+            setStatus("Converged at generation " + generation + " with cost " +  intf.format(result.cost) + "; Rate: " + doubf.format(result.rate));
+            update();
           }
-          double avg = sum/convergenceHistory.size();
-          double sumOfSquares = -1;
-          for(double i : convergenceHistory) {
-              double diff = Math.abs(i - avg);
-              sumOfSquares += diff * diff;
-          }
-          double stdDev = Math.sqrt(sumOfSquares);
-          if(stdDev < 50) {
-              setStatus("Converged at generation " + generation);
-              update();
-              break;
-          }
+          result.convergenceGen = generation;
+          return result;
+        }
       }
-      setStatus("Generation " + generation + " Cost " + (int) thisCost);
 
-      update();
+      //evolution rate calculation
+      long currentGenTime = System.currentTimeMillis() - genStartTime;
+      if(timingHistory.size() == HISTORY_SIZE) {
+        timingHistory.remove();
+      }
+      timingHistory.add(currentGenTime);
+      if(timingHistory.size() == HISTORY_SIZE) {
+        double sum = 0;
+        for(double i : timingHistory) {
+          sum += i;
+        }
+        result.rate = (HISTORY_SIZE * 1000.0) / sum;
+      }
 
+      if(graphical) {
+        setStatus("Generation " + generation + " Cost " + intf.format(result.cost) + " Rate " + doubf.format(result.rate));
+        update();
+      }
     }
-    setStatus("Solution found after " + generation + " generations.");
+    if(graphical) {
+      setStatus("Ran out of generations at " + generation + ". Cost " + intf.format(result.cost) + " Rate " + doubf.format(result.rate));
+      update();
+    }
+    return result;
   }
 
-  @Override
-  public void paint(Graphics g) {
-	  update();
+  private class RunResult {
+    public double rate = 0;
+    public int convergenceGen = 0;
+    public double cost = 0;
+    public void add(RunResult other) {
+      this.rate += other.rate;
+      this.convergenceGen += other.convergenceGen;
+      this.cost += other.cost;
+    }
+    public void divide(double factor) {
+      this.rate /= factor;
+      this.convergenceGen /= factor;
+      this.cost /= factor;
+    }
   }
 }
+// vim: ts=2:sw=2
